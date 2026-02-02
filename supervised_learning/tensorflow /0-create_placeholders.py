a0c29cf8ffd18e6586ac801be29d0baace009efc
Vinny <vicentmugabo75@gmail.com>
2026-02-02 11:54:37 +0200

    created pl

---
+++ supervised_learning/tensorflow /0-create_placeholders.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+
+
+def create_placeholders(nx, classes):
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
+    y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
+
+    return x, y
+
