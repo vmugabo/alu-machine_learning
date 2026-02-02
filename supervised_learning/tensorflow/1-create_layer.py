ef3fd5eec4c144eed01798d54e871a4c0fa838c0
Vinny <vicentmugabo75@gmail.com>
2026-02-02 12:13:53 +0200

    update for pycodestyle

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""Creates a neural network layer using TensorFlow."""
 import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
-    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    """Create a fully connected layer with variance scaling initialization."""
+    initializer = tf.contrib.layers.variance_scaling_initializer(
+        mode="FAN_AVG")
     layer = tf.layers.dense(
         prev,
         n,
