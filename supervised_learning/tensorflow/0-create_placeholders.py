2f706b6b6990bdd0723661e2aaed605c5cafd583
Vinny <vicentmugabo75@gmail.com>
2026-02-02 12:17:44 +0200

    error resolved

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 
 def create_placeholders(nx, classes):
     """Create placeholders for input features and target classes."""
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
     y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
 
     return x, y
